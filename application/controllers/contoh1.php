50fc335e37a7a06e47433f5e1f181c3281b788bf
Syahwal28 <rizkisyahwal28@gmail.com>
2024-03-25 18:35:38 +0700

    tugas 1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -1,12 +1,13 @@
 <?php
 class contoh1 extends CI_Controller
 {
-    public function index()
+    public function index ()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Rizki Syahwal Ludiansyah
-            Saya tinggal di desa jawa tengah kecamatan sungai ambawang
-            Hobi saya bermain bulutangkis
-            ";
+        echo "Nama saya Rizki Syahwal Ludiansyah
+              Saya tinggal di desa jawa tengah kecamatan sungai ambawang
+              Hobi saya bermain bulutangkis
+              ";
     }
 }
+
