fb3540e5489f382816210646676b41526cc4c17b
Syahwal28 <rizkisyahwal28@gmail.com>
2024-04-22 16:13:30 +0700

    Pertemuan 4 dan Tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rizki Syahwal</li>
+        <li>Nama Belakang : Ludiansyah</li>
+    </ul>
+   
+
+    <h4>Alamat</h4>
+    <ul type="circle">
+        <li>Sungai Ambawang</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kubu Raya</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Maen game</li>
+    </ul>
+</section>
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+        <footer>
+            <a href="http://www.RentalBuku.com"></a>
+        </footer>
+    </div>
+</body>
+</html>
