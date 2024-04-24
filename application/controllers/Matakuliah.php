ad74d5d5574e431c1e22bb24fa07faddf027eb18
Syahwal28 <rizkisyahwal28@gmail.com>
2024-04-24 10:37:50 +0700

    Pertemuan 5 dan Tugas 4

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -8,12 +8,24 @@ class Matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $data=[
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
-
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 
+        'required[min_length[3]',['required'=> 'Kode Matakuliah Harus Diisi', 'min_length'=> 'Kode Terlalu pendek']);
+      
+        $this->form_validation->set_rules('nama', 'Kode Matakuliah', 
+        'required[min_length[3]',['required'=> 'Kode Matakuliah Harus Diisi', 'min_length'=> 'Kode Terlalu pendek']);
+        
+        $this->form_validation->set_rules('sks', 'SKS', 
+        'required',['required'=> 'SKS Harus Diisi']);
+        
+        if ($this->form_validation->run() !=true) {
+            $this->load->view('view-form-matakuliah');
+        }else{
+            $data=[
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];     
         $this->load->view('view-data-matakuliah', $data);
+        }
     }
 }
